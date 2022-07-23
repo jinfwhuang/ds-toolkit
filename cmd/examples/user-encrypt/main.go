74b84d7d1ca4cb3f7f248c44fee38cd26c8c2203
Kaloyan Tanev <tanevdev@gmail.com>
2022-07-23 20:39:22 +0200

    Add comments and printlines, small refactor in protobuf

--- cmd/examples/user-encrypt/main.go
+++ cmd/examples/user-encrypt/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
@@ -12,42 +13,61 @@ import (
 )
 
 const (
-	// secp256k1 key
+	// Seeded secp256k1 private key.
 	testPrivkeyHex = "12de257b783b96ce90012a6c45f3ce61216dd60f22159d2f5cb9e17f3126bbe5"
 )
 
+// Get the running directory of the current file.
 func fileInRuntimeDir(file string) string {
 	_, filename, _, _ := runtime.Caller(0)
 	return filepath.Dir(filename) + file
 }
 
 func main() {
+	// -ENCRYPTING-
+	// Initialize *ecdsa.PrivateKey, using seeded hex private key.
+	// In real environment the user is expected to input the data.
 	alicePrivateKey, err := ethereum.HexToECDSA(testPrivkeyHex)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Printf("Using public key %v of initial owner", &alicePrivateKey.PublicKey)
 
-	exampleData, err := ioutil.ReadFile(fileInRuntimeDir("/data.json"))
+	// Load the data the user wants to encrypt.
+	dataPath := "/data.json"
+	fmt.Printf("Using %v as example data blob", dataPath)
+	exampleData, err := ioutil.ReadFile(fileInRuntimeDir(dataPath))
 	if err != nil {
 		log.Fatal("could not find data file")
 	}
 
+	// Create encrypted data blob for the user, using user's public key.
+	fmt.Println("Encrypting data blob")
 	dataBlob, err := ds.CreateDataBlob(exampleData, &alicePrivateKey.PublicKey)
 	if err != nil {
 		panic(err)
 	}
+
+	// Create JSON struct from the encrypted data blob.
+	dataBlobPath := "/data_blob.json"
+	fmt.Printf("Saving the encrpyed data blob to %v", dataBlobPath)
 	dataBlobJson, err := json.Marshal(dataBlob)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(fileInRuntimeDir("/data_blob.json"), dataBlobJson, 0644)
+
+	// Write the encrypted data blob to a JSON file.
+	err = ioutil.WriteFile(fileInRuntimeDir(dataBlobPath), dataBlobJson, 0644)
 	if err != nil {
 		log.Fatal("could not find data file")
 	}
 
+	// Decrypt the data from the encrypted data blob, using user's private key.
 	retrievedData, err := ds.ExtractData(dataBlob, alicePrivateKey)
 	if err != nil {
 		panic(err)
 	}
-	println(string(retrievedData))
+	// Print the initial data blob
+	fmt.Printf("Decrypted encrpyed data blob, using owner's private key: %v", testPrivkeyHex)
+	fmt.Println(string(retrievedData))
 }
--- cmd/examples/user-share/main.go
+++ cmd/examples/user-share/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"path/filepath"
@@ -14,21 +15,30 @@ import (
 )
 
 const (
-	// secp256k1 keys
+	// Seeded secp256k1 private key of the current owner, used to decrypt the initial encrypted data blob.
 	testPrivkeyHex = "12de257b783b96ce90012a6c45f3ce61216dd60f22159d2f5cb9e17f3126bbe5"
-	testPubkeyHex  = "0237dcc127cd98c4080fbb82f148e4bb4281153276acfa718b3366b74c4a1039bb"
+	// Seeded secp256k1 public key of the owner to be added.
+	testPubkeyHex = "0237dcc127cd98c4080fbb82f148e4bb4281153276acfa718b3366b74c4a1039bb"
 )
 
+// Get the running directory of the current file.
 func fileInRuntimeDir(file string) string {
 	_, filename, _, _ := runtime.Caller(0)
 	return filepath.Dir(filename) + file
 }
 
 func main() {
+	// --
+	// Initialize *ecdsa.PrivateKey, using seeded hex private key.
+	// In real environment the user is expected to input the data.
+	fmt.Printf("Using private key %v for decryption", testPrivkeyHex)
 	alicePrivateKey, err := ethereum.HexToECDSA(testPrivkeyHex)
 	if err != nil {
 		panic(err)
 	}
+	// Initialize *ecdsa.PublicKey, using seeded hex public key.
+	// In real environment the user is expected to input the data.
+	fmt.Printf("Using public key %v for new shared owner", testPrivkeyHex)
 	bobPublicKeyHex, err := hex.DecodeString(testPubkeyHex)
 	if err != nil {
 		panic(err)
@@ -38,7 +48,10 @@ func main() {
 		panic(err)
 	}
 
-	blobBytes, err := ioutil.ReadFile(fileInRuntimeDir("/data_blob.json"))
+	// Load the data the user wants to encrypt from a JSON file.
+	dataBlobPath := "/data_blob.json"
+	fmt.Printf("Using %v as example encrypted data blob", dataBlobPath)
+	blobBytes, err := ioutil.ReadFile(fileInRuntimeDir(dataBlobPath))
 	if err != nil {
 		panic("could not find blob file")
 	}
@@ -48,22 +61,32 @@ func main() {
 		panic(err)
 	}
 
+	// Add the new shared owner, using its public key.
+	// Create new encrypted data blob.
+	fmt.Println("Decrypting encrypted data blob and adding new owner's key")
 	newDataBlob, err := ds.AddKey(&dataBlob, bobPublicKey, alicePrivateKey)
 	if err != nil {
 		panic(err)
 	}
+	// Marshal the new encrypted data blob to JSON byte slice.
 	newDataBlobJson, err := json.Marshal(newDataBlob)
 	if err != nil {
 		panic(err)
 	}
-	err = ioutil.WriteFile(fileInRuntimeDir("/new_data_blob.json"), newDataBlobJson, 0644)
+	// Write the new encrypted data blob to a JSON file.
+	newDataBlobPath := "/new_data_blob.json"
+	fmt.Printf("Saving the new encrpyed data blob to %v", newDataBlobPath)
+	err = ioutil.WriteFile(fileInRuntimeDir(newDataBlobPath), newDataBlobJson, 0644)
 	if err != nil {
 		log.Fatal("could not create data file")
 	}
 
+	// Decrypt the data from the encrypted data blob, using initial owner's private key.
 	retrievedData, err := ds.ExtractData(newDataBlob, alicePrivateKey)
 	if err != nil {
 		panic(err)
 	}
-	println(string(retrievedData))
+	// Print the new data blob
+	fmt.Println("Decrypted new encrpyed data blob, using initial owner's private key")
+	fmt.Println(string(retrievedData))
 }
