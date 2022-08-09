ee1fdab425e747fa2e59e7ecbd62916fd5461868
Kaloyan Tanev <tanevdev@gmail.com>
2022-08-09 18:49:01 +0200

    Ceramic test

---
+++ experimental/dsn/ceramic/ceramic.go
@@ -0,0 +1,94 @@
+package ceramic
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const (
+	// Used for local development, see https://developers.ceramic.network/build/cli/api/ for more.
+	// nodeUrl = "http://0.0.0.0:7007"
+	nodeUrl = "https://ceramic-clay.3boxlabs.com"
+	didKey  = "YOUR_DID_KEY_CREATED_USING_GLAZE_OR_JS_CLIENT"
+)
+
+type AnchorProof struct {
+	Root           string `json:"root"`
+	TxHash         string `json:"txHash"`
+	ChainId        string `json:"chainId"`
+	BlockNumber    int    `json:"blockNumber"`
+	BlockTimestamp int    `json:"blockTimestamp"`
+}
+
+type Log struct {
+	Cid       string `json:"cid"`
+	Type      int    `json:"type"`
+	Timestamp int    `json:"timestamp,omitempty"`
+}
+
+type Metadata struct {
+	Unique      string   `json:"unique,omitempty"`
+	Controllers []string `json:"controllers"`
+}
+
+type State struct {
+	Type         int         `json:"type"`
+	Content      string      `json:"content"`
+	Metadata     Metadata    `json:"metadata"`
+	Signature    int         `json:"signature"`
+	AnchorStatus string      `json:"anchorStatus"`
+	Log          []Log       `json:"log"`
+	AnchorProof  AnchorProof `json:"anchorProof"`
+	DocType      string      `json:"docType"`
+}
+
+type Stream struct {
+	StreamId string `json:"streamId"`
+	State    State  `json:"state"`
+}
+
+// Does not work!
+// Data needs to be signed in order to be recognised. However, there is no documentation on what kind of signature should be used, neither in what field it should be put.
+// Works without issues using the ceramic CLI (check https://developers.ceramic.network/build/cli/quick-start/).
+// Currently it seems the platform is made to be used only via the CLI or the JS Client.
+// Probably it is possible to reverse engineer how the JS Client works (or the CLI), but it would take too much time.
+func Write(data string) (string, error) {
+	url2 := fmt.Sprintf("%v/api/v0/streams", nodeUrl)
+
+	jsonBody := []byte(fmt.Sprintf(`{
+		"type": 0,
+		"genesis": {
+			"header": {
+				"family": "test",
+				"controllers": ["did:key:%v"]
+			},
+			"data": "%v"
+		}
+	}`, didKey, data))
+	bodyReader := bytes.NewReader(jsonBody)
+
+	respJson, err := http.Post(url2, "application/json", bodyReader)
+	if err != nil {
+		return "", err
+	}
+
+	var resp Stream
+	json.NewDecoder(respJson.Body).Decode(&resp)
+
+	return resp.StreamId, nil
+}
+
+func Read(streamId string) (string, error) {
+	url := fmt.Sprintf("%v/api/v0/streams/%v", nodeUrl, streamId)
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", err
+	}
+
+	var res Stream
+	err = json.NewDecoder(resp.Body).Decode(&res)
+	return res.State.Content, err
+}
--- experimental/dsn/main.go
+++ experimental/dsn/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/jinfwhuang/ds-toolkit/experimental/dsn/arweave"
+	"github.com/jinfwhuang/ds-toolkit/experimental/dsn/ceramic"
 	estuary "github.com/jinfwhuang/ds-toolkit/experimental/dsn/filecoin-estuary"
 	"github.com/jinfwhuang/ds-toolkit/experimental/dsn/storj"
 )
@@ -16,27 +17,27 @@ func main() {
 
 func arweaveExample() {
 	id, err := arweave.Write(data)
-	println(id)
 	if err != nil {
 		println(err.Error())
 	}
+	println(id)
 
 	// This is an example id that should be retrieved from Write function.
 	// Arweave takes couple of minutes until the id is accepted from the chain,
 	// that is why we use another random id that is already in the chain here.
 	ret, err := arweave.Read("IUYBL-mW7OpG7Em_kwIpucrg43Br64nGbeMM01yja4w")
-	println(string(ret))
 	if err != nil {
 		println(err.Error())
 	}
+	println(string(ret))
 }
 
 func estuaryExample() {
 	resp, err := estuary.Write(data)
-	println(resp)
 	if err != nil {
 		println(err.Error())
 	}
+	println(resp)
 }
 
 func storjExample() {
@@ -47,8 +48,25 @@ func storjExample() {
 		println(err.Error())
 	}
 
-	retr, _ := storj.Read(key)
+	retr, err := storj.Read(key)
+	if err != nil {
+		println(err.Error())
+	}
 	println(string(retr))
+}
+
+func ceramicExample() {
+	streamId, err := ceramic.Write(string(data))
+	if err != nil {
+		println(err.Error())
+	}
+	println(streamId)
+
+	// This is an example id of a Stream that should be obtained from the above call.
+	// However, the writing via HTTP does not seem to be maintained and currently does not work.
+	// The example ID is an ID of a stream generated via the CLI.
+	retr, err := ceramic.Read("kjzl6cwe1jw147z6im9aagzi92icf72n7evjt5kdhevd97ygkaxjqbicyxz618i")
+	println(retr)
 	if err != nil {
 		println(err.Error())
 	}
