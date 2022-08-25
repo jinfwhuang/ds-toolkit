c9c02eec7498fe8859d6998cf3b61788b7b27a7e
Kaloyan Tanev <tanevdev@gmail.com>
2022-08-25 20:41:42 +0200

    Improve testing

--- go-pkg/dsn/arweave_test.go
+++ go-pkg/dsn/arweave_test.go
@@ -1,13 +1,51 @@
 package dsn
 
 import (
+	b64 "encoding/base64"
+	"io/ioutil"
 	"testing"
 
+	"github.com/everFinance/goar"
+	"github.com/everFinance/goar/types"
 	"github.com/stretchr/testify/assert"
 )
 
+var test_wallet string = "./test_wallet.json"
+
 func TestGenerateWallet(t *testing.T) {
-	b, err := GenerateJWK()
+	w, err := GenerateWallet()
+	assert.NoError(t, err)
+	assert.NotNil(t, w)
+
+	walletAssertions(t, w)
+}
+
+func TestGenerateWalletFromPath(t *testing.T) {
+	w, err := GenerateWalletFromPath(test_wallet)
+	assert.NoError(t, err)
+	assert.NotNil(t, w)
+	walletAssertions(t, w)
+}
+
+func TestGenerateWalletFromJWK(t *testing.T) {
+	b, err := ioutil.ReadFile(test_wallet)
 	assert.NoError(t, err)
 	assert.NotNil(t, b)
+
+	w, err := GenerateWalletFromJWK(b)
+	assert.NoError(t, err)
+	assert.NotNil(t, w)
+
+	walletAssertions(t, w)
+}
+
+func walletAssertions(t *testing.T, w *goar.Wallet) {
+	data := []byte{2}
+	dataBase64 := b64.RawURLEncoding.EncodeToString(data)
+
+	item, err := w.CreateAndSignBundleItem(data, 1, "dGVzdA", "anchor", []types.Tag{})
+	assert.NoError(t, err)
+	assert.NotNil(t, item.Data)
+	assert.NotNil(t, item.Signature)
+	assert.Equal(t, dataBase64, item.Data)
 }
